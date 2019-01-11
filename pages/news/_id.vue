3d1b14d50e5c01d0e930a22d680b66269f7db49a
Jesus Urrutia <jesus.urrutia@gmail.com>
2019-01-11 18:12:37 -0300

    feat(pages): added news detail page

---
+++ pages/news/_id.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h1>{{ data.title }}</h1>
+
+    <div>{{ data.body }}</div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+
+export default {
+  asyncData({ params, ...context }) {
+    const { id } = params;
+
+    return axios({
+      url: `https://jsonplaceholder.typicode.com/posts/${id}`,
+    })
+      .then(res => res.data)
+      .then(data => ({ data }))
+    ;
+  },
+
+  head() {
+    return {
+      title: this.data.title,
+    };
+  },
+}
+</script>
+
